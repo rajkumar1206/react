f0ffb4c5276667213482052b10ff05f6941236a0
rajkumar1206 <57241375+rajkumar1206@users.noreply.github.com>
2020-04-11 00:17:04 -0700

    Updates

---
+++ precache-manifest.acd5f8a7feb7ed36432317ef849ee7ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c98c984f3197912319c64aaa814708",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "c9f9e357a47e3bef1d43",
+    "url": "/react/static/css/main.4bf3cff3.chunk.css"
+  },
+  {
+    "revision": "2f0b4d1190cf36c9310a",
+    "url": "/react/static/js/2.f6327ffb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.f6327ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f9e357a47e3bef1d43",
+    "url": "/react/static/js/main.a9d22882.chunk.js"
+  },
+  {
+    "revision": "d79789f038f71c4ee3bb",
+    "url": "/react/static/js/runtime-main.c79e98d3.js"
+  }
+]);
